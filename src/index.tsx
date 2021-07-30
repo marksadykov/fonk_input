d0af4dcd06f2773fc2ec8721a4eb61357f402ff5
Mark Sadykov <sadykov.mark@gmail.com>
2021-07-30 16:07:16 +0300

    setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import TestDiv from './uicomponents/TestDiv';
+import React from "react"
+import ReactDOM from "react-dom"
+import TestDiv from "./uicomponents/TestDiv"
 
 const App = () => (
-    <div><h1>Тестовый компонент</h1><TestDiv /></div>
-);
+  <div>
+    <h1>Тестовый компонент</h1>
+    <TestDiv />
+  </div>
+)
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
